8ed225e50006f9950f6af48714d13c7398b86cfe
mahdiheidarabadi <mahdiheidarabadi06@gmail.com>
2025-09-19 12:06:26 +0330

    version1

---
+++ main.js
@@ -0,0 +1,21 @@
+const countri = document.getElementById("countri");
+
+fetch("https://restcountries.com/v3.1/all?fields=name,capital,flags,population,region,cca3"
+).then((res) => {
+    return res.json()
+}).then((data) => {
+    data.forEach((value) => {
+        countri.innerHTML += `<li>
+        <div>
+<img src=${value.flags.png} alt="">        
+        </div>
+        <div>
+        <span>${value.name.common}</span>
+         <p>popolashan:${value.population} </p>
+        <p>ragen:${value.ragion}</p>
+        <p>capital: ${value.capital[0]}</p>
+        </div>
+        </li>`
+
+    })
+})
